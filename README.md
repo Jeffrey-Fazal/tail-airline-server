0ebaba2ba2ed69440c8415bc0b3520f3efdb0db2
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-07 21:58:42 +1100

    see endpoints in readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-## This is a group project
+# Burning Airline Server
+
+The Server is used to host the database and provide endpoints for the data. Endpoints are in json format
+
+## Endpoints
+
+http://localhost:3000/reservations.json
+http://localhost:3000/flights.json
+http://localhost:3000/airplanes.json
+http://localhost:3000/users.json
+
+## TODO:
+
+* Change authenticy token to allow communication with react
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-Airplane.destroy_all
-
-a1 = Airplane.create(name: 'AAYk42',rows:'[A]',columns:'[1]')
-a2 = Airplane.create(name: '32E32E',rows:'[C]',columns:'[2]')
-a3 = Airplane.create(name: 'LIK021',rows:'[F]',columns:'[2]')
-a4 = Airplane.create(name: '291IEJ',rows:'[B]',columns:'[4]')
 
 Flight.destroy_all
 # f0 = Flight.create(flight_number: 'SYDAUHAM', origin: 'SYD', destination: 'AUD', date: '2023-05-08 20:53:01.000000000 +1100', airplane_id: 1)
@@ -11,6 +5,7 @@ f1 = Flight.create(flight_number: 'SYDAUHAM', origin: 'SYD', destination: 'AUD',
 f2 = Flight.create(flight_number: 'QLDWAAPM', origin: 'QLD', destination: 'WAH', date: '2023-05-09 20:53:01.000000000 +1100', airplane_id: 1)
 f3 = Flight.create(flight_number: 'ADLNTHAM', origin: 'ADL', destination: 'NTO', date: '2023-04-03 20:53:01.000000000 +1100', airplane_id: 2)
 
+puts "Created #{Flight.count} flights"
 
 User.destroy_all
 
@@ -19,10 +14,24 @@ u2 = User.create(username: 'Hu', is_admin: true, email: 'hu@ga.com')
 u3 = User.create(username: 'jane', is_admin: false, email: 'jane@ga.com')
 u4 = User.create(username: 'jhon', is_admin: false, email: 'doe@ga.com')
 
+puts "Created #{User.count} users"
+
 Reservation.destroy_all
 
-r1 = Reservation.create(user_id: '',flight_id: '',seat: '')
-r2 = Reservation.create(user_id: '',flight_id: '',seat: '')
-r3 = Reservation.create(user_id: '',flight_id: '',seat: '')
+r1 = Reservation.create(user_id: '1',flight_id: '1',seat: '[A1]')
+r2 = Reservation.create(user_id: '1',flight_id: '1',seat: '[B3]')
+r3 = Reservation.create(user_id: '2',flight_id: '2',seat: '[D4]')
+
+puts "Created #{Reservation.count} reservations"
+
+Airplane.destroy_all
+
+a1 = Airplane.create(name: 'AAYk42',rows:'[A]',columns:'[1]')
+a2 = Airplane.create(name: '32E32E',rows:'[C]',columns:'[2]')
+a3 = Airplane.create(name: 'LIK021',rows:'[F]',columns:'[2]')
+a4 = Airplane.create(name: '291IEJ',rows:'[B]',columns:'[4]')
+
+puts "Created #{Airplane.count} airlines"
+
 
 # TODO: create database association records
