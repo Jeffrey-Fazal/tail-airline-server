7be9a006f7c3f2acdc3d3f10a60676438d82374d
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-08 14:56:47 +1100

    changed and tested date

---
+++ db/migrate/20230208035138_flights_change_column_date_datatype_date.rb
@@ -0,0 +1,5 @@
+class FlightsChangeColumnDateDatatypeDate < ActiveRecord::Migration[5.2]
+  def change
+    change_column :flights, :date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_055338) do
+ActiveRecord::Schema.define(version: 2023_02_08_035138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2023_02_07_055338) do
     t.text "flight_number"
     t.text "origin"
     t.text "destination"
-    t.datetime "date"
+    t.date "date"
     t.integer "airplane_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
